2cf0483019fc2db065ae4f45502e99b21810609d
nayeemmehedi <boyneel79@gmail.com>
2021-01-30 22:16:25 +0600

    Booking Flights js

--- work.js
+++ work.js
@@ -2,36 +2,36 @@
 
 
 
- function ChangeFirstClass(isIncrease){
+function ChangeFirstClass(isIncrease) {
 
-    let add_input =  document.getElementById("add_count")
-    
-    let add_input1 = parseInt(add_input.value )
-    let add_new_count = add_input1
+  let add_input = document.getElementById("add_count")
 
-    if (isIncrease ==true){
+  let add_input1 = parseInt(add_input.value)
+  let add_new_count = add_input1
 
-         add_new_count = add_input1 + 1
+  if (isIncrease == true) {
 
-    }
-    if(isIncrease==false){
-         add_new_count = add_input1 - 1
-    }
-   
- 
-    if (add_new_count>=0){
+    add_new_count = add_input1 + 1
 
-        add_input.value = add_new_count
-      }
+  }
+  if (isIncrease == false) {
+    add_new_count = add_input1 - 1
+  }
 
 
+  if (add_new_count >= 0) {
+
+    add_input.value = add_new_count
+  }
 
 
-      total()
 
-     
 
- }
+  total()
+
+
+
+}
 
 
 
@@ -39,95 +39,96 @@
 //  Economy function
 
 
- function changeEconomyClass(isIncreaseEco){
+function changeEconomyClass(isIncreaseEco) {
 
-    const add_input =  document.getElementById("add_count_eco")
-    const add_input1 = parseInt(add_input.value)
+  const add_input = document.getElementById("add_count_eco")
+  const add_input1 = parseInt(add_input.value)
 
-    let  add_new_count = add_input1
+  let add_new_count = add_input1
 
-    if (isIncreaseEco==true){
-        add_new_count = add_input1 + 1
+  if (isIncreaseEco == true) {
+    add_new_count = add_input1 + 1
 
-    }
-    if (isIncreaseEco==false){
-        add_new_count = add_input1 - 1
+  }
+  if (isIncreaseEco == false) {
+    add_new_count = add_input1 - 1
 
-    }
-     
-    if(add_new_count>=0){
+  }
+
+  if (add_new_count >= 0) {
 
     add_input.value = add_new_count
 
- }
+  }
 
- total()
+  total()
 }
 
 
 // total price function
 
 
-function total(){
-  const firstClassF =  document.getElementById("add_count")
-  const firstClassInput =parseInt(firstClassF.value)
+function total() {
+  const firstClassF = document.getElementById("add_count")
+  const firstClassInput = parseInt(firstClassF.value)
 
-  const EcoClassF =  document.getElementById("add_count_eco")
-  const EcoClassInput =parseInt(EcoClassF.value)
-  
-
-
-const totalPrice_value = firstClassInput * 150 +  EcoClassInput*100
-
-document.getElementById("totalPrice").innerText = "$"+ totalPrice_value
+  const EcoClassF = document.getElementById("add_count_eco")
+  const EcoClassInput = parseInt(EcoClassF.value)
 
 
- let vat_value= (totalPrice_value *10) /100
 
- document.getElementById("vat_id").innerText ="$" + vat_value
+  const totalPrice_value = firstClassInput * 150 + EcoClassInput * 100
 
- let last_total_value = totalPrice_value + vat_value
+  document.getElementById("totalPrice").innerText = "$" + totalPrice_value
 
 
- document.getElementById("lastTotal").innerText ="$" + last_total_value
- 
-   make_voucher()
-   
+  let vat_value = (totalPrice_value * 10) / 100
+
+  document.getElementById("vat_id").innerText = "$" + vat_value
+
+  let last_total_value = totalPrice_value + vat_value
+
+
+  document.getElementById("lastTotal").innerText = "$" + last_total_value
+
+  make_voucher()
+
 }
 
 //voucher page 
 
-function make_voucher(){
+function make_voucher() {
 
-    const firstClassF =  document.getElementById("add_count")
-  const firstClassInput =parseInt(firstClassF.value)
+  const firstClassF = document.getElementById("add_count")
+  const firstClassInput = parseInt(firstClassF.value)
 
-  const EcoClassF =  document.getElementById("add_count_eco")
-  const EcoClassInput =parseInt(EcoClassF.value)
-  
-
-
-const totalPrice_value = firstClassInput * 150 +  EcoClassInput*100
-
-document.getElementById("totalPrice1").innerText = "$"+ totalPrice_value
+  const EcoClassF = document.getElementById("add_count_eco")
+  const EcoClassInput = parseInt(EcoClassF.value)
 
 
- let vat_value= (totalPrice_value *10) /100
 
- document.getElementById("vat_id1").innerText ="$" + vat_value
+  const totalPrice_value = firstClassInput * 150 + EcoClassInput * 100
 
- let last_total_value = totalPrice_value + vat_value
- 
+  document.getElementById("total_Price").innerText = "$" + totalPrice_value
 
- document.getElementById("lastTotal1").innerText ="$" + last_total_value
- 
-  let first_input =document.getElementById("add_count").value
-//   console.log(first_input)
+
+  let vat_value = (totalPrice_value * 10) / 100
+
+  document.getElementById("vat_id_num").innerText = "$" + vat_value
+
+  let last_total_value = totalPrice_value + vat_value
+
+
+  document.getElementById("last_Total").innerText = "$" + last_total_value
+
+  let first_input = document.getElementById("add_count").value
+
+  //   console.log(first_input)
   document.getElementById("first_class_num").innerText = first_input
 
 
 
-  let economic_input =document.getElementById("add_count_eco").value
+  let economic_input = document.getElementById("add_count_eco").value
   console.log(economic_input)
 
   document.getElementById("economic_num").innerText = economic_input
@@ -141,17 +142,17 @@ document.getElementById("totalPrice1").innerText = "$"+ totalPrice_value
 
 const first_display = document.getElementById("login");
 first_display.addEventListener("click", function () {
-    const login_area = document.getElementById("first_div");
-    login_area.style.display = "none";
+  const login_area = document.getElementById("first_div");
+  login_area.style.display = "none";
 
-    const second_display = document.getElementById("second_div");
-    second_display.style.display = "block";
+  const second_display = document.getElementById("second_div");
+  second_display.style.display = "block";
 
-    document.getElementById("add_count").value =0
+  document.getElementById("add_count").value = 0
 
 
-    document.getElementById("add_count_eco").value =0
-    
+  document.getElementById("add_count_eco").value = 0
+
 
 });
 
